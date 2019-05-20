62053b9056226d9a2b0e8cfba7988f6ef8c56b0f
ivasconcelos <ikelisonva.jmzafra@gmail.com>
2019-05-20 19:14:09 +0200

    Porra

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.ArrayList;
+
+public class Main {
+    static BufferedReader reader;
+    public static Object ArrayList;
+
+    public static void main(String[] args) throws IOException {
+        reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<Aposta> bet = new ArrayList<Aposta>(0);
+    }
+}
